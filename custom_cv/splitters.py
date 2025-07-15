295a7548d6f425eed975bc77937f5d5a8717d762
Akinsanya Joel <akinsanyajoel82@gmail.com>
2025-07-15 02:46:17 +0100

    Implement TimeSeriesCV

--- custom_cv/splitters.py
+++ custom_cv/splitters.py
@@ -50,3 +50,59 @@ class StratifiedKFoldCV:
   def get_n_splits(self, X=None, y=None, groups=None):
       """Returns the number of splitting iterations."""
       return self.n_splits
+
+
+
+class TimeSeriesCV:
+   """
+   Custom Time-Series cross-validator.
+   Creates folds that respect the temporal order of the
+   data.
+   The training set always come befor the test set.
+   """
+   def __init__(self, n_splits = 5, test_size = 1, gap = 0):
+      """
+      Args:
+      n_splits (int): The number of splits/folds.
+      test_size (int): The number of samples in each test set.
+      gap (int): The number of samples to skip between the end of the train set
+      and the beginning of the test set. This prevents leakage from the data points
+      that might be too cose in time.
+      """
+      if n_splits <= 1:
+         raise ValueError("n_splits must be greater than 1.")
+
+      self.n_splits = n_splits
+      self.test_size = test_size
+      self.gap = gap
+
+   def split(self, X, y = None, groups = None):
+      """
+      Generates Indices to split data into training
+      and test set
+      """
+      n_samples = len(X)
+      indices = np.arange(n_samples)
+
+      total_test_plus_gaps = (self.n_splits * self.test_size) + ((self.n_splits - 1) * self.gap)
+      first_test_start = n_samples - total_test_plus_gaps
+
+      if first_test_start <= 0:
+         raise ValueError(f"Cannot create {self.n_splits} folds with test_size={self.test_size}"
+                          f"and gap={self.gap}. The dataset of size {n_samples} is too small.")
+
+      for i in range (self.n_splits):
+         test_start = first_test_start + i * (self.test_size + self.gap)
+         test_end = test_start + self.test_size
+         train_end = test_start - self.gap
+         train_indices = indices[0:train_end]
+         test_indices = indices[test_start:test_end]
+
+         if len(train_indices) == 0:
+            raise ValueError("The first training set has 0 samples. adjust parameters.")
+
+         yield train_indices,test_indices
+
+   def get_n_splits(self, X=None, y=None, groups=None):
+      """Returns the number of splitting iterations."""
+      return self.n_splits
--- example.py
+++ example.py
@@ -3,12 +3,15 @@ from sklearn.datasets import make_classification
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
-from custom_cv.splitters import StratifiedKFoldCV
+
+from custom_cv.splitters import StratifiedKFoldCV, TimeSeriesCV
 
 print("Testing our Custom StratifiedKFoldCV")
 
-# 1. Create imbalanced dummy data
+# Create imbalanced dummy data
 X, y = make_classification(
     n_samples=150, n_features=5, n_informative=3, n_redundant=0,
     n_classes=3, n_clusters_per_class=1,
@@ -17,10 +20,10 @@ X, y = make_classification(
 )
 print(f"Overall Class Distribution: {np.bincount(y)}")
 
-# 2. Create an instance of our cross-validator
+#  Create an instance of our cross-validator
 custom_cv = StratifiedKFoldCV(n_splits=5, shuffle=True, random_state=42)
 
-# 3. Use the splitter
+# Use the splitter
 model = LogisticRegression()
 fold_scores = []
 
@@ -37,6 +40,44 @@ for fold_num, (train_indices, test_indices) in enumerate(custom_cv.split(X, y)):
     score = accuracy_score(y_test, predictions)
     fold_scores.append(score)
 
-# 4. Print final results
+# Print final results
 print("\n Final Results")
 print(f"Mean Accuracy: {np.mean(fold_scores):.4f} (+/- {np.std(fold_scores):.4f})")
+
+
+
+print("Testing our Custom TimeSeriesCV")
+
+n_samples = 50
+X_time = np.arange(n_samples).reshape(-1, 1)
+
+y_time = X_time.flatten() * 2 + np.random.rand(n_samples) * 5
+
+ts_cv = TimeSeriesCV(n_splits=5,test_size=3,gap=2)
+
+print(f"Splitting {n_samples} samples into {ts_cv.get_n_splits()} folds:")
+model = LinearRegression()
+fold_scores = []
+
+for fold_num, (train_indices, test_indices) in enumerate(ts_cv.split(X_time)):
+    print(f"\nFold {fold_num+1}:")
+    print(f" Train indices: {train_indices}")
+    print(f" Test indices: {test_indices}")
+
+    if train_indices.size > 0:
+        print(f"  --> Max train index ({train_indices.max()}) is less than min test index ({test_indices.min()})")
+
+    # Train the model and get score
+    X_train, X_test = X_time[train_indices], X_time[test_indices]
+    y_train, y_test = y_time[train_indices], y_time[test_indices]
+
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+    # For regression, we often use Mean Squared Error (lower is better)
+    score = mean_squared_error(y_test, predictions)
+    fold_scores.append(score)
+
+# Print final results
+print("\n--- Final Time-Series Results ---")
+print(f"Mean Squared Error for each fold: {[f'{s:.2f}' for s in fold_scores]}")
+print(f"Average MSE: {np.mean(fold_scores):.2f}")
